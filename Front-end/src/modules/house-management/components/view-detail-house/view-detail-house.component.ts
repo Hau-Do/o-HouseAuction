6814ee02332ae71896ac9ba220da40f40ed12c72
vinh <vinh220195@gmail.com>
2017-12-26 07:52:39 +0700

    commit detail house component

---
+++ Front-end/src/modules/house-management/components/view-detail-house/view-detail-house.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'view-detail-house-component',
+  templateUrl: './view-detail-house.component.html',
+  styleUrls: ['./view-detail-house.component.css']
+})
+
+export class ViewDetailHouseComponent {
+  
+}
