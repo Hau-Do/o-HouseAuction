a58e6aaf3974effb793513b3c33c5f347ed2a99a
Hau-Do <haudotien@gmail.com>
2018-01-06 16:14:59 +0700

    Dem nay em dep la vi ai. Neu la vi anh thi dem nay ben anh nha

--- Front-end/src/modules/house-management/house-management.module.ts
+++ Front-end/src/modules/house-management/house-management.module.ts
@@ -7,6 +7,7 @@ import { houseManagementRouter } from './house-management.routes';
 import { HouseManagementComponent } from './house-management.component';
 import { ViewHouseComponent } from './components/view-house/view-house.component';
 import { PostHouseComponent } from './components/post-house/post-house.component';
+import { ViewDetailHouseComponent } from './components/view-detail-house/view-detail-house.component';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import { PostHouseComponent } from './components/post-house/post-house.component
   declarations: [
     HouseManagementComponent,
     ViewHouseComponent,
-    PostHouseComponent
+    PostHouseComponent,
+    ViewDetailHouseComponent
   ]
 })
 export class HouseManagementModule { }
