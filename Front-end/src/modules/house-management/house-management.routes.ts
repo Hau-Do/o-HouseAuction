c1e466d950820f19471c6ce97131812ca9cd543c
Hau-Do <haudotien@gmail.com>
2018-01-06 17:45:16 +0700

    updated payment module

--- Front-end/src/modules/house-management/house-management.routes.ts
+++ Front-end/src/modules/house-management/house-management.routes.ts
@@ -1,17 +1,24 @@
 import { RouterModule } from '@angular/router';
 
 import { Routes } from '@angular/router';
-import { ViewHouseComponent } from './components/view-house/view-house.component';
+import { ViewDetailHouseComponent } from './components/view-detail-house/view-detail-house.component';
 import { PostHouseComponent } from './components/post-house/post-house.component';
 import { HouseManagementComponent } from './house-management.component';
 
 
 const HOUSE_MANAGEMENT_ROUTE: Routes = [
-    // { path: 'view-house', component: ViewHouseComponent },
-    // { path: 'post-house', component: PostHouseComponent },
     {
         path: '',
-        component: HouseManagementComponent
+        component: HouseManagementComponent,
+        children: [
+            {
+                path: '',
+                children: [
+                    { path: 'house', component: ViewDetailHouseComponent },
+                    { path: 'post', component: PostHouseComponent }
+                ]
+            }
+        ]
     }
 ];
 
