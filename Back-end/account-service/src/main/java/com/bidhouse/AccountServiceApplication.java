5c3c5ea099bdb26060cd5b9eeccbd0735830514c
Bao Toan <baotoan95>
2018-02-15 02:12:29 +0700

    Improve microservices architecture to prepare for secure

--- Back-end/account-service/src/main/java/com/bidhouse/AccountServiceApplication.java
+++ Back-end/account-service/src/main/java/com/bidhouse/AccountServiceApplication.java
@@ -2,13 +2,13 @@ package com.bidhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.bidhouse.listener.CascadeSaveMongoEventListener;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
