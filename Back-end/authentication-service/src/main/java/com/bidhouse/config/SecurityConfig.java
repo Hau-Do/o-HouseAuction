2d53fe9cafd1a6d1f4ada7f498fba2c697fa5995
Bao Toan <baotoan95>
2018-02-20 16:23:06 +0700

    Apply SSO

---
+++ Back-end/authentication-service/src/main/java/com/bidhouse/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.bidhouse.config;
+
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().loginPage("/login").permitAll()
+        .and()
+        .authorizeRequests()
+            .anyRequest().authenticated().and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+}
--- Back-end/house-service/src/main/java/com/bidhouse/HouseServiceApplication.java
+++ Back-end/house-service/src/main/java/com/bidhouse/HouseServiceApplication.java
@@ -2,12 +2,14 @@ package com.bidhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
+@EnableResourceServer
 public class HouseServiceApplication {
 
 	public static void main(String[] args) {
